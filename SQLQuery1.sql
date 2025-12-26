725514fb66c058af3335aaf779bc8b52c37ba068
Tika Khatiashvili <tikaxatiashvili1997@gmail.com>
2025-12-26 13:10:43 +0400

    Add SQLQuery1 script

---
+++ SQLQuery1.sql
@@ -0,0 +1,18 @@
+﻿USE testDB;
+GO
+
+-- Users ცხრილი
+CREATE TABLE Users (
+    UserId INT PRIMARY KEY,
+    UserName NVARCHAR(50) NOT NULL
+);
+
+-- UserProfiles ცხრილი (One-to-One)
+CREATE TABLE UserProfiles (
+    UserId INT PRIMARY KEY,
+    BirthDate DATE,
+    City NVARCHAR(50),
+    CONSTRAINT FK_UserProfiles_Users
+        FOREIGN KEY (UserId)
+        REFERENCES Users(UserId)
+);
